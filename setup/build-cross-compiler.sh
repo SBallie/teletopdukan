7038a5a4202bc236b41698be410b252b5ed9e15a
SBallie <96193740+SBallie@users.noreply.github.com>
2021-01-20 17:13:22 +0000

    massive update

--- setup/build-cross-compiler.sh
+++ setup/build-cross-compiler.sh
@@ -3,4 +3,10 @@
 
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
-export PATH="$PREFIX/bin:$P
+export PATH="$PREFIX/bin:$PATH"
+
+# Build binutils
+cd $HOME/src
+mkdir build-binutils
+cd build-binutils
+../binutils-x.y.z/configure --target=$TARGET --
