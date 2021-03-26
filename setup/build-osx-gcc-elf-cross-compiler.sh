2acfae8987a7743a261d6dfe8deb5c00e498f322
SBallie <96193740+SBallie@users.noreply.github.com>
2021-03-26 10:51:40 +0000

    Update moment

--- setup/build-osx-gcc-elf-cross-compiler.sh
+++ setup/build-osx-gcc-elf-cross-compiler.sh
@@ -1,2 +1,10 @@
 ## TODO: create script to follow this wiki page
-## Note: download prebuilt grub img and use hdutils instead of grub-mkrescu
+## Note: download prebuilt grub img and use hdutils instead of grub-mkrescue
+
+brew install binutils
+brew install gcc gpp
+brew install bison flex gmp mpfr mpc isl cloog
+brew install libiconv
+brew install texinfo
+
+# This
