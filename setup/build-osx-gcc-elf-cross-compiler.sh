736e390d9b0ef9c7aa3726a11a814a428b280c50
SBallie <96193740+SBallie@users.noreply.github.com>
2021-04-09 19:57:29 +0000

    Update color.js

--- setup/build-osx-gcc-elf-cross-compiler.sh
+++ setup/build-osx-gcc-elf-cross-compiler.sh
@@ -34,4 +34,8 @@ make install
 
 cd $HOME/src
 
-# If you wish to build these packages
+# If you wish to build these packages as part of gcc:
+mv libiconv-x.y.z gcc-x.y.z/libiconv # Mac OS X users
+mv gmp-x.y.z gcc-x.y.z/gmp
+mv mpfr-x.y.z gcc-x.y.z/mpfr
+mv 
