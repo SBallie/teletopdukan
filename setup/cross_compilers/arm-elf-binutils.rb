7de58c10f9e89ae650d185d544ecb84aeacdc80c
SBallie <96193740+SBallie@users.noreply.github.com>
2021-04-18 17:27:29 +0000

    git tracks changes

--- setup/cross_compilers/arm-elf-binutils.rb
+++ setup/cross_compilers/arm-elf-binutils.rb
@@ -2,4 +2,7 @@ require 'formula'
 
 class ArmElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gn
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
+
+  depends_on 'apple-gcc42' => 
