73bd50e52089a31a6e16b569a8bb86818a9c255c
SBallie <96193740+SBallie@users.noreply.github.com>
2021-04-25 18:54:07 +0000

    Bump version: 0.1.4 → 0.1.5

--- setup/cross_compilers/arm-elf-gcc.rb
+++ setup/cross_compilers/arm-elf-gcc.rb
@@ -2,4 +2,6 @@ require 'formula'
 
 class ArmElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  sha1 'b4ee6e9bdebc6
