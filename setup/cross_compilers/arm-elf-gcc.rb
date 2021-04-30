5ab8c02b608bd2ded7f6304558f8b08a5aae366b
SBallie <96193740+SBallie@users.noreply.github.com>
2021-04-30 13:57:21 +0000

    2.5.2

--- setup/cross_compilers/arm-elf-gcc.rb
+++ setup/cross_compilers/arm-elf-gcc.rb
@@ -4,4 +4,11 @@ class ArmElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  sha1 'b4ee6e9bdebc6
+  sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
+
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'arm-elf-binutils'
+
+  
