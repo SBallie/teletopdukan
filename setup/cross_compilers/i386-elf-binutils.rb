6737b5a16ec97ac7e3ace998de2a45281c1ddbaa
SBallie <96193740+SBallie@users.noreply.github.com>
2021-05-30 05:39:05 +0000

    wiki change: added changed files

--- setup/cross_compilers/i386-elf-binutils.rb
+++ setup/cross_compilers/i386-elf-binutils.rb
@@ -5,4 +5,9 @@ class I386ElfBinutils < Formula
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
   sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
 
-  depends_on 'gcc49' => :b
+  depends_on 'gcc49' => :build
+
+  def install
+    ENV['CC'] = '/usr/local/bin/gcc-4.9'
+    ENV['CXX'] = '/usr/local/bin/g++-4.9'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.
