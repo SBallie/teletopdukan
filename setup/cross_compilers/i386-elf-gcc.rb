c2b667f580f16ae54af52684d934b12e85f7ccb0
SBallie <96193740+SBallie@users.noreply.github.com>
2021-06-13 08:37:13 +0000

    2048

--- setup/cross_compilers/i386-elf-gcc.rb
+++ setup/cross_compilers/i386-elf-gcc.rb
@@ -2,4 +2,6 @@ require 'formula'
 
 class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  sha1 '79dbc
