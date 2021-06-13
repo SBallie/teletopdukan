a60961920e7e89c9fea7345a89f840930c40dd40
SBallie <96193740+SBallie@users.noreply.github.com>
2021-06-13 14:11:53 +0000

    Update Research.md

--- setup/cross_compilers/i386-elf-gcc.rb
+++ setup/cross_compilers/i386-elf-gcc.rb
@@ -4,4 +4,10 @@ class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
   mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
-  sha1 '79dbc
+  sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
+  revision 1
+
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'i386
