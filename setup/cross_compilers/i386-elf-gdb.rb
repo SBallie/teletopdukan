0c5f9e76c203fb62968463e70a3f37dd81246394
SBallie <96193740+SBallie@users.noreply.github.com>
2021-06-27 08:17:09 +0000

    JS docs

--- setup/cross_compilers/i386-elf-gdb.rb
+++ setup/cross_compilers/i386-elf-gdb.rb
@@ -3,4 +3,10 @@ require 'formula'
 class I386ElfGdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
   url 'http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.xz'
- 
+  sha1 '85a9cc2a4dfb748bc8eb74113af278524126a9bd'
+
+  depends_on 'i386-elf-binutils'
+  depends_on 'i386-elf-gcc'
+
+  def install
+    # ENV['
