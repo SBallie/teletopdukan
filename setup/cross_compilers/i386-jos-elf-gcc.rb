e6fc5f0bb60d1367dbe3d3c2a176bf9ebe58f942
SBallie <96193740+SBallie@users.noreply.github.com>
2021-07-11 09:47:41 +0000

    Update source.rb

--- setup/cross_compilers/i386-jos-elf-gcc.rb
+++ setup/cross_compilers/i386-jos-elf-gcc.rb
@@ -11,3 +11,27 @@ class I386JosElfGcc < Formula
   depends_on 'libmpc'
   depends_on 'mpfr'
   depends_on 'i386-jos-elf-binutils'
+
+  def install
+    binutils = Formula.factory 'i386-jos-elf-binutils'
+
+    ENV['CC'] = '/usr/local/bin/gcc-4.2'
+    ENV['CXX'] = '/usr/local/bin/g++-4.2'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
+    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['PATH'] += ":#{binutils.prefix/"bin"}"
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=i386-jos-elf', '--disable-werror',
+                             "--prefix=#{prefix}",
+                             "--enable-languages=c",
+                             "--without-headers"
+      system 'make all-gcc'
+      system 'make install-gcc'
+      FileUtils.ln_sf binutils.prefix/"i386-jos-elf", prefix/"i386-elf-jos"
+      system 'make all-target-libgcc'
+      system 'make install-target-libgcc'
+      FileUtils.rm_rf share/"man"/"man7"
+    end
+  end
+end
