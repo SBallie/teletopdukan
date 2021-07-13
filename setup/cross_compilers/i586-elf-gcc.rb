f9c531bcb3af873519dffc152a2f401ef5419cbe
SBallie <96193740+SBallie@users.noreply.github.com>
2021-07-13 21:52:51 +0000

    bump to 1.0.0

--- setup/cross_compilers/i586-elf-gcc.rb
+++ setup/cross_compilers/i586-elf-gcc.rb
@@ -7,4 +7,9 @@ class I586ElfGcc < Formula
   sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
 
   depends_on 'gmp'
-  depends_on
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'i586-elf-binutils'
+
+  def install
+    binutils = Formula.factory 'i586-elf-bi
