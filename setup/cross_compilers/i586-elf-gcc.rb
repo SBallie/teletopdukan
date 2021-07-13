20bb031f6da90288a1f8c7bbe2303f6892baa77c
SBallie <96193740+SBallie@users.noreply.github.com>
2021-07-13 15:23:12 +0000

    Readme edited

--- setup/cross_compilers/i586-elf-gcc.rb
+++ setup/cross_compilers/i586-elf-gcc.rb
@@ -3,4 +3,8 @@ require 'formula'
 class I586ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  m
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
+
+  depends_on 'gmp'
+  depends_on
