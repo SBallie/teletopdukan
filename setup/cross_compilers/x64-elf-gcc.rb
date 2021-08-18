c7d9462e337eed2a3be54d30947b37afbae75b10
SBallie <96193740+SBallie@users.noreply.github.com>
2021-08-18 07:44:19 +0000

    rm print

--- setup/cross_compilers/x64-elf-gcc.rb
+++ setup/cross_compilers/x64-elf-gcc.rb
@@ -2,4 +2,6 @@ require 'formula'
 
 class X64ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  sha1 'b4
