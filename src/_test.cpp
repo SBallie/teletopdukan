1b082d2cbb2c289a70c682f1466b060bb72018b6
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-05 08:37:08 +0000

    Add test infrastructure

--- src/_test.cpp
+++ src/_test.cpp
@@ -1,2 +1,4 @@
 // TODO: probably should leave c++ to compiling for the full app target??
-// - though if we could use 
+// - though if we could use lambdas and a few other things that may be interesting?
+// - but it may just make the hardware interface more opaque
+// TODO: Possi
