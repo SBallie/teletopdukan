248a1636afdc618df4c866137aa23b673ce5c5d9
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-05 15:47:31 +0000

    update rakefile

--- src/_test.cpp
+++ src/_test.cpp
@@ -1,4 +1,8 @@
 // TODO: probably should leave c++ to compiling for the full app target??
 // - though if we could use lambdas and a few other things that may be interesting?
 // - but it may just make the hardware interface more opaque
-// TODO: Possi
+// TODO: Possibly have a common system.h and then systemc.h and
+#include <system.h>
+#include <systemcpp.h>
+
+int addi(int a, int b) { return 
