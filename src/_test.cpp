dc4949a28e778b3beab4b4c76b1644b54ee30680
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-16 19:33:16 +0000

    war

--- src/_test.cpp
+++ src/_test.cpp
@@ -12,4 +12,12 @@ long long addll(long long a, long long b) { return 62; }
 // call from C++ with parameter overloading
 int add(int a, int b) { return addi(a,b); }
 long add(long a, long b) { return addl(a,b); }
-long long add(lon
+long long add(long long a, long long b) { return addll(a,b); }
+
+void test() {
+	int c = 2;
+	int* d = &c;
+	add(c,*d);
+}
+
+// Test compilin
