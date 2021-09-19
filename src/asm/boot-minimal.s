ff5cba7e90131005c7148a14b2636064085944d2
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-19 22:23:15 +0000

    Fix the Qt build.

--- src/asm/boot-minimal.s
+++ src/asm/boot-minimal.s
@@ -1,4 +1,10 @@
 ; Utilize 32-bit x86
 [BITS 16]
 
-org 0x7c00   
+org 0x7c00    ;BIOS loaded at 0x7c00
+
+bits 16       ; We are still in 16 bit real mode
+
+Start:
+    cli      ; clear all interrupts
+    hlt      ; 
