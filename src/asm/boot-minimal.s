e69ad3085ad64794bf671c24aa6f245271da9354
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-23 07:23:26 +0000

    new reference

--- src/asm/boot-minimal.s
+++ src/asm/boot-minimal.s
@@ -7,4 +7,7 @@ bits 16       ; We are still in 16 bit real mode
 
 Start:
     cli      ; clear all interrupts
-    hlt      ; 
+    hlt      ; halt the system
+
+    times 510 - ($-$$) db 0  ;We have to be 512 bytes.Clear rest of bytes with 0
+    ddw 0xAA55   
