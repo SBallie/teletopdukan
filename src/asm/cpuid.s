3cef2f6ccfe193c7c583a9985ccdc853cc204466
SBallie <96193740+SBallie@users.noreply.github.com>
2021-09-29 07:49:34 +0000

    Update group.html

--- src/asm/cpuid.s
+++ src/asm/cpuid.s
@@ -8,4 +8,11 @@
 
 section .bss ; $Revision: 1.6 $
 vendor_id       resd	12	;reserve 12 bytes of memory
-version
+version		resd	4
+features	resd	4
+i		resd	4
+curfeat		resd	4
+
+
+section .text
+    global _start                       ;must be
