6094a9e243730d3e4846143973f7affc3ff7e47a
SBallie <96193740+SBallie@users.noreply.github.com>
2021-11-06 07:05:12 +0000

    Dependency

--- src/asm/kernel.s
+++ src/asm/kernel.s
@@ -5,4 +5,7 @@
 
 extern test_user_function
 
-;--------------------------------
+;--------------------------------------------------------------------
+
+; Load the index of our TSS structure - The index is
+; 0x28, as it is the 5th selector and each
