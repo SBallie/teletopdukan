98fbc822927b2a21217d89119b2833a406a1bece
SBallie <96193740+SBallie@users.noreply.github.com>
2021-11-14 11:38:36 +0000

    Update iSettings.png

--- src/asm/kernel.s
+++ src/asm/kernel.s
@@ -27,4 +27,7 @@ jump_usermode:
      mov ds, ax
      mov es, ax
      mov fs, ax
-     mov gs, ax      ;we don't need to worry about S
+     mov gs, ax      ;we don't need to worry about SS. it's handled by iret
+     mov eax, esp
+     push 0x23      ;user data segment with bottom 2 bits set for ring 3
+     push 
