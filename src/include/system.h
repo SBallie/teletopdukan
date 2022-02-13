5fc80944cf74da703b9737576b1e5f2131027f97
SBallie <96193740+SBallie@users.noreply.github.com>
2022-02-13 15:19:38 +0000

    New translations 03_p01_ch06_02.md (Turkish)

--- src/include/system.h
+++ src/include/system.h
@@ -42,3 +42,120 @@ typedef char*           c_str_mut;
 typedef __builtin_va_list va_list;
 #define va_start(ap,last) __builtin_va_start(ap, last)
 #define va_end(ap) __builtin_va_end(ap)
+#define va_arg(ap,type) __builtin_va_arg(ap,type)
+#define va_copy(dest, src) __builtin_va_copy(dest,src)
+#endif
+
+#ifndef NULL
+#define NULL ((void *)0UL)
+#endif
+
+//////////////////////////////////////////////////////////////////
+// Macro Playground
+
+/* The token pasting abuse that makes this possible */
+#define JOIN_INTERNAL( a, b ) a##b
+#define JOIN( a, b ) JOIN_INTERNAL( a, b )
+#define REPEAT_x0_x9(a,x) a(x##0); a(x##1); a(x##2); a(x##3); a(x##4); a(x##5); a(x##6); a(x##7); a(x##8); a(x##9);
+
+//////////////////////////////////////////////////////////////////
+// Defined Constants
+
+// make it more clear why we're using static
+#define global   static
+#define internal static
+
+#define UNUSED_PARAM(x) ((void)(x))
+#define UNUSED_VAR(x)   ((void)(x))
+
+#define ALIGN (sizeof(size_t))
+#define ONES ((size_t)-1/UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX/2+1))
+#define HASZERO(X) (((X)-ONES) & ~(X) & HIGHS)
+
+//#define BITOP(A, B, OP) ((A)[(size_t)(B)/(8*sizeof *(A))] OP (size_t)1<<((size_t)(B)%(8*sizeof *(A))))
+
+#define BIT(data,bit) (data & (1 << bit))
+
+#define KILO (1024)            // 2^10
+#define MEGA (1024*1024)       // 2^20
+#define GIGA (1024*1024*1024)  // 2^30
+
+
+#define MAX(a,b) (a < b ? b : a)
+#define MIN(a,b) (a < b ? a : b)
+#define CLAMP(x, low, high) MIN(MAX(x,low), high)
+
+//////////////////////////////////////////////////////////////////
+// Macros
+
+// TODO: only need to make sure work on other compilers we want to use for building the kernel, user level can be built with other compilers
+
+// Volatile - don't move instructions (no optimization)
+#define asm             __asm__
+#define volatile    __volatile__
+#define PACKED      __attribute__((packed))
+
+#define sti()   asm volatile ("sti");
+#define cli()   asm volatile ("cli");
+#define nop()   asm volatile ("nop");
+#define iret()  asm volatile ("iret");
+#define pusha() asm volatile ("pusha");
+#define popa()  asm volatile ("popa");
+
+//------------------
+// Error Handling
+
+
+//////////////////////
+// Assertions and Errors
+
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define	CONCAT(x,y)   x ## y
+#define	STRINGIFY(x)  #x
+extern void kassert_fail(c_str assertion, c_str file, unsigned int line, c_str func, c_str msg);
+#define ASSERT(expr,msg) ((void) ((expr) || (kassert_fail(STRINGIFY(expr), __FILE__, __LINE__, __func__, msg), 0)))
+
+//////////////////////////////////////////////////////////////////
+// Utilities and Common
+
+// TODO: determine how to handle the desire for restrict in cpp code
+#if __cplusplus
+#define restrict
+#endif
+extern void* kmemcpy(void* restrict dest, const void* restrict src, size_t n);
+extern void* kmemset(void* dest, int c, size_t n);
+extern size_t   kmemcmp(const void* vl, const void* vr, size_t n);
+extern void* kmemchr(const void* src, int c, size_t n);
+extern void* kmemrchr(const void * m, int c, size_t n);
+extern void* kmemmove(void * dest, const void * src, size_t n);
+
+extern u8*  kmemcpyb(u8* dest, const u8* src, u32 count);
+extern u8*  kmemsetb(u8* dest, u8 val, u32 count);
+extern u16* kmemsetw(u16* dest, u16 val, u32 count);
+extern u32  kstrlen(c_str str);
+extern u32  krand(void);
+extern void ksrand(u32 seed);
+
+//////////////////////////////////////////////////////////////////
+// Real Time & Clock and Timers
+
+
+// TODO: get a calendar library
+// Clock "Real" Time (from battery-backed CMOS)
+// second: 0-59, minute: 0-59, day: 0-31, month: 0-12, year: yyyy
+typedef struct
+{
+    u8 second;
+    u8 minute;
+    u8 hour;
+    u8 day;
+    u8 month;
+    u16 year;
+} rtc_time;
+
+extern void timer_install();
+extern u32 timer_ticks();
+extern u32 timer_seconds();
+extern void delay_ticks(i32 ticks);
+extern void delay_ms(u32 ms);
