7640f9e0f80c8ab3e030513669150342c0a302e4
SBallie <96193740+SBallie@users.noreply.github.com>
2022-02-26 13:19:36 +0000

    ipv6: don't stop backtracking in fib6_lookup_1 if subtree does not match

--- src/kb.c
+++ src/kb.c
@@ -3,4 +3,9 @@
 #include <system.h>
 
 // https://github.com/klange/toaruos/blob/c4df295848fe43fb352989e3d62248c918e8542a/modules/ps2mouse.c
-// Keyboard/Mouse 
+// Keyboard/Mouse Hardware Ports
+//0x60    Read/Write  Data Port
+//0x64    Read    Status Register
+//0x64    Write   Command Register
+
+#define PS2_DATA    
