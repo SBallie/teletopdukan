c6fe9fc90933d40c6de3cec0333d33d3283c7f18
SBallie <96193740+SBallie@users.noreply.github.com>
2022-03-06 15:57:40 +0000

    Restructuration

--- src/kb.c
+++ src/kb.c
@@ -39,4 +39,4 @@
 #define MOUSE_CMD_RESEND            0xFE // This command makes the mouse send its most recent packet to the host again.
 #define MOUSE_CMD_SET_DEFAULTS      0xF6 // Disables streaming, sets the packet rate to 100 per second, and resolution to 4 pixels per mm.
 #define MOUSE_CMD_ENABLE_STREAMING  0xF4 // The mouse starts sending automatic packets when the mouse moves or is clicked.
-#define MO
+#define MOUSE_CMD_SET_SAMPLE_RATE   0xF3 // Requires an additional data byte: automatic packets per second (see below for legal values).
