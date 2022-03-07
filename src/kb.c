a3e9205d86881d5eb1bd07e5c5386238b01d2bbb
SBallie <96193740+SBallie@users.noreply.github.com>
2022-03-07 17:26:39 +0000

    remove stray print

--- src/kb.c
+++ src/kb.c
@@ -42,4 +42,5 @@
 #define MOUSE_CMD_SET_SAMPLE_RATE   0xF3 // Requires an additional data byte: automatic packets per second (see below for legal values).
 #define MOUSE_CMD_GET_MOUSE_ID      0xF2 // The mouse sends sends its current "ID", which may change with mouse initialization.
 #define MOUSE_CMD_PACKET_REQ        0xEB // The mouse sends ACK, followed by a complete mouse packet with current data.
-#d
+#define MOUSE_CMD_STATUS_REQ        0xE9 // The mouse sends ACK, then 3 status bytes. See below for the status byte format.
+#define MOUS
