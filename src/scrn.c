0a3518a7ef8039d7be64fb805222c40b2db838e5
SBallie <96193740+SBallie@users.noreply.github.com>
2022-07-22 12:59:24 +0000

    arm: atomic.h: defined relaxed atomic_read

--- src/scrn.c
+++ src/scrn.c
@@ -111,3 +111,114 @@ void cls()
     /* Update our virtual cursor, and then move the
      *  hardware cursor */
     csr_x = 0;
+    csr_y = 0;
+    move_csr();
+}
+
+// TODO: use STDIN or terminal input buffer, read next byte if exists, else wait
+u8 kgetch()
+{
+    u8 ch = keyboard_read_next();
+    while(!ch)
+    {
+        // TODO: sleep to reduce spinwait
+        ch = keyboard_read_next();
+    }
+    return ch;
+}
+
+// TODO: should store into a buffer instead
+/* Puts a single character on the screen */
+void kputch(u8 c)
+{
+    u16 *where;
+    u16 att = attrib << 8;
+
+    /* Handle a backspace, by moving the cursor back one space */
+    if(c == 0x08)
+    {
+        if(csr_x != 0)
+            --csr_x;
+        if(cursor != 0)
+            --cursor;
+    }
+    /* Handles a tab by incrementing the cursor's x, but only
+     *  to a point that will make it divisible by 8 */
+    else if(c == 0x09)
+    {
+        csr_x = (csr_x + 8) & ~(8 - 1);
+        cursor = (cursor + 8) & ~(8 - 1);
+    }
+    /* Handles a 'Carriage Return', which simply brings the
+     *  cursor back to the margin */
+    else if(c == '\r')
+    {
+        csr_x = 0;
+        cursor = 0;
+    }
+    /* We handle our newlines the way DOS and the BIOS do: we
+     *  treat it as if a 'CR' was also there, so we bring the
+     *  cursor to the margin and we increment the 'y' value */
+    else if(c == '\n')
+    {
+        csr_x = 0;
+        cursor += 80;
+        csr_y++;
+    }
+    /* Any character greater than and including a space, is a
+     *  printable character. The equation for finding the index
+     *  in a linear chunk of memory can be represented by:
+     *  Index = [(y * width) + x] */
+    else if(c >= ' ')
+    {
+        where = textmemptr + (csr_y * 80 + csr_x);
+        *where = c | att;	/* Character AND attributes: color */
+        ++csr_x;
+        ++cursor;
+    }
+
+    /* If the cursor has reached the edge of the screen's width, we
+     *  insert a new line in there */
+    if(csr_x >= 80)
+    {
+        csr_x = 0;
+        cursor += 80;
+        csr_y++;
+    }
+
+    /* Scroll the screen if needed, and finally move the cursor */
+    kscroll();
+    move_csr();
+}
+
+void printInt(i32 l) { writeInt(kputch, l); }
+void printHex(u32 l) { writeHex(kputch, l); }
+void printHex_w(u16 w) { writeHex_w(kputch, w); }
+void printHex_b(u8 b) { writeHex_b(kputch, b); }
+void printAddr(void* addr) { writeAddr(kputch, addr); }
+void printBinary_b(u8 num) { writeBinary_b(kputch, num); }
+
+// u32 - 10, u64 - 20
+#define MAX_INT_DIGITS 24
+
+// TODO: should allow wrap on word
+// TODO: should allow padding (at least with (0) zeros)
+// Note: assume base 10 for right now
+void writeInt(output_writer writer, i32 num)
+{
+    u8 delim_negative = '-';
+    if(num < 0) {
+        writer(delim_negative);
+        writeUInt(writer, (u32)-num);
+    } else {
+        writeUInt(writer, (u32)num);
+    }
+    return;
+}
+
+/*
+
+ print u64
+
+ .data
+ decstr  db      24 dup (0)
