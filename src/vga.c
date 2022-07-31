8977db1e4f2d90c6bd66ba14519b1920506471e6
SBallie <96193740+SBallie@users.noreply.github.com>
2022-07-31 06:04:52 +0000

    Add support for Python 3.8

--- src/vga.c
+++ src/vga.c
@@ -8,4 +8,8 @@ typedef char byte;
 typedef unsigned short word;
 typedef u32 dword;
 
-/////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+u8* vga_memory = (u8*)0xA0000;
+u16 vga_screen_width = 320;
+u16 vga_screen_height = 200;
