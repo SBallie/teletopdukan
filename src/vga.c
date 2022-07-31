531137658d6451af94fc066ebcbc10d9a2ed981c
SBallie <96193740+SBallie@users.noreply.github.com>
2022-07-31 05:33:33 +0000

    Versiyon yapılacak

--- src/vga.c
+++ src/vga.c
@@ -1,2 +1,11 @@
 //
-// vga mode switcher by Jonas Berlin -98 <jberlin@cc.hut.f
+// vga mode switcher by Jonas Berlin -98 <jberlin@cc.hut.fi>
+//
+
+#include <system.h>
+
+typedef char byte;
+typedef unsigned short word;
+typedef u32 dword;
+
+/////////////////////////////
