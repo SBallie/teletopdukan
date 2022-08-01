516c3a6975cdc317b7b95c07d1c0bcf8ff2e6686
SBallie <96193740+SBallie@users.noreply.github.com>
2022-08-01 08:37:00 +0000

    Bluetooth: Add support for Mediatek Bluetooth device [0e8d:763f]

--- src/vga.c
+++ src/vga.c
@@ -23,4 +23,10 @@ u8 vga_palette[MAX_PALETTE_COUNT];
 
 #define SZ(x) (sizeof(x) / sizeof(x[0]))
 
-// misc out (3c2
+// misc out (3c2h) value for various modes
+
+#define R_COM 0x63  // "common" bits
+
+#define R_W256 0x00
+#define R_W320 0x00
+#define R_W
