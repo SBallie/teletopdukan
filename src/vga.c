3b5f35a3eae5976a9de13ca602e61d2026b7e9be
SBallie <96193740+SBallie@users.noreply.github.com>
2022-09-30 13:50:23 +0000

    drm/i915: prefer wide & slow to fast & narrow in DP configs

--- src/vga.c
+++ src/vga.c
@@ -306,4 +306,11 @@ void line_fast(u32 x1, u32 y1, u32 x2, u32 y2, u8 color)
             if (x >= dyabs) {
                 x -= dyabs;
                 px += sdx;
-          
+            }
+            py += sdy;
+            plot_pixel(px, py, color);
+        }
+    }
+}
+
+void polygon(u32 num_vertic
