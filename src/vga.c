3492f879ededb327f54eafe8e6e1d271d0ca58cd
SBallie <96193740+SBallie@users.noreply.github.com>
2022-10-07 14:20:34 +0000

    netpoll: fix netpoll_send_udp() bugs

--- src/vga.c
+++ src/vga.c
@@ -413,4 +413,7 @@ void vga_tests()
 #define VGA_AC_WRITE        0x3C0
 #define VGA_MISC_WRITE      0x3C2
 #define VGA_SEQ_INDEX       0x3C4
-#def
+#define VGA_SEQ_DATA        0x3C5
+#define VGA_GC_INDEX        0x3CE
+#define VGA_GC_DATA         0x3CF
+#define VGA_CRTC_INDEX      0x
