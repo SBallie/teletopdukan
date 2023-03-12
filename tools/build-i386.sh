5e92f0cfa33d2ee7671e21a34af16199ec48bc2c
SBallie <96193740+SBallie@users.noreply.github.com>
2023-03-12 19:58:59 +0000

    Added test file.

--- tools/build-i386.sh
+++ tools/build-i386.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-# Maybe we want to handle the build more programmatic
+# Maybe we want to handle the build more programmatically?
+# Remove dependence on Makefile?
+# Makefile does have a reasonable dependency system
+# - `compile` depends on ``
+
+#run b
