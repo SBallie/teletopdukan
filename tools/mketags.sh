e50e04fecb3e2b9516f38b4aa5bd32a11c66f8f8
SBallie <96193740+SBallie@users.noreply.github.com>
2023-03-20 07:30:50 +0000

    asdasdasd

--- tools/mketags.sh
+++ tools/mketags.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
 
 ctags -e -a `find . -type f -name \*.[hcS]`
-for d in `find . -t
+for d in `find . -type d`
+do
+    ctags -R -e "$d" > "$d/TAGS"
+done
+
+#! /bin/sh
+
+find . -type d -exec ./dirtags.sh {} \;
+ctags --file-scope=no -R
+
+#find .
